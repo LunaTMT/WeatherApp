7b8a54a4b85453b3d5aac580f079c7a0f71f42c4
LunaTMT <taylorthreader@gmail.com>
2024-05-11 14:37:55 +0100

    Finally add local time zone given city (lat/lon), API call does not work correctly with egypt, something wrong with openweather API

--- script.js
+++ script.js
@@ -10,67 +10,32 @@ search_button.addEventListener('click', function() {
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=b65a5b81ad984ccc91d2c11d02cc3578")
     .then(response => response.json())
     .then(data => {
-
+        
+        console.clear();
         console.log(data.name)
 
         console.log(data.weather[0].main);
         console.log(data.weather[0].description);
         console.log(data.main.temp);
         
-
-        console.log(data.timezone)
-
-        let unix_timestamp = data.dt;
-        let timezone_offset = data.timezone;
+        console.log(data.sys.country);
+        let latitude = data.coord.lat;
+        let longitude = data.coord.lon;
         
-        var date = new Date(unix_timestamp * 1000);
-        var utc = date.getTime() + (date.getTimezoneOffset() * 60000);
-        var local_time = new Date(utc + (timezone_offset * 1000));
-
-
-        console.log(local_time);
-
-
-
-        // Construct the URL for geocoding API request
-        var geocodingUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${API_KEY}`;
-
-        // Make a GET request to geocoding API to get latitude and longitude
-        fetch(geocodingUrl)
+        fetch(`http://api.timezonedb.com/v2.1/get-time-zone?key=YWF4V0PKH9ZN&format=json&by=position&lat=${latitude}&lng=${longitude}`)
         .then(response => response.json())
-        .then(data => {
-            // Extract latitude and longitude from the geocoding API response
-            var latitude = data.results[0].geometry.location.lat;
-            var longitude = data.results[0].geometry.location.lng;
-
-            // Get current timestamp (in seconds)
-            var timestamp = Math.floor(Date.now() / 1000);
-
-            // Construct the URL for time zone API request
-            var timezoneUrl = `https://maps.googleapis.com/maps/api/timezone/json?location=${latitude},${longitude}&timestamp=${timestamp}&key=${API_KEY}`;
-
-            // Make a GET request to time zone API
-            return fetch(timezoneUrl);
+        .then(data => {   
+            console.log(data.formatted);  
         })
-        .then(response => response.json())
-        .then(data => {
-            // Extract timezone information from the time zone API response
-            var timezoneId = data.timeZoneId;
-            var timezoneName = data.timeZoneName;
-            var localTime = new Date((timestamp + data.rawOffset + data.dstOffset) * 1000);
-
-            console.log("Timezone ID:", timezoneId);
-            console.log("Timezone Name:", timezoneName);
-            console.log("Local Time:", localTime);
-        })
-        .catch(error => console.error('Error:', error));
+        .catch(error => {
+            console.error('Error: ', error);
+        });
+    
+       
     })
     .catch(error => {
         console.error('Error: ', error);
     });
 
 });
-
-
-
 
